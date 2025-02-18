85c9c0522bd7ecaa0870d56c77ad7cb1367a7932
rohrsben <rohrsben@gmail.com>
2025-02-18 14:51:38 -0500

    feat (swww): image preview in swww-set

--- users/error/shared/apps/fish.nix
+++ users/error/shared/apps/fish.nix
@@ -58,35 +58,37 @@
             swww-set = {
                 body = ''
                     argparse 'd' 'l' 'p' 's' -- $argv
-                    if set -ql _flag_d
-                        echo "Setting default backgrounds."
-                        swww img -o DP-3 -t random --transition-fps 120 ~/xdg/pictures/backgrounds/primary.*
-                        swww img -o DP-1 -t random --transition-fps 120 ~/xdg/pictures/backgrounds/secondary.*
-                        return 0
-                    end
+
+                    set PRIMARY DP_3
+                    set SECONDARY DP_1
 
                     set search_dir ~/xdg/pictures/backgrounds
+
+                    if set -ql _flag_d
+                        echo "Setting default backgrounds."
+                        swww img -o (echo $PRIMARY | sed -e 's/_/-/g') -t random --transition-fps 120 $search_dir/primary.*
+                        swww img -o (echo $SECONDARY | sed -e 's/_/-/g') -t random --transition-fps 120 $search_dir/secondary.*
+                        return 0
+                    end
 
                     if set -ql _flag_l
                         set search_dir .
                     end
 
                     if set -ql _flag_p
-                        set -a monitors PRIMARY
-                        set PRIMARY DP_3
+                        set -a to_change PRIMARY
                     end
 
                     if set -ql _flag_s
-                        set -a monitors SECONDARY
-                        set SECONDARY DP_1
+                        set -a to_change SECONDARY
                     end
 
-                    for monitor in $monitors
-                        set $$monitor (ls $search_dir | rg -i '.png|.jpg' | fzf --prompt "$monitor > " --height ~30%)
+                    for monitor in $to_change
+                        set $$monitor (fd -e png -e jpg --base-directory $search_dir | fzf --prompt "$monitor > " --height 30% --min-height 15 --preview "kitten icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place=\"\$FZF_PREVIEW_COLUMNS\"x\"\$FZF_PREVIEW_LINES\"@0x0 $search_dir/{}")
                     end
 
-                    for monitor in $monitors
-                        if test -n $$$monitor
+                    for monitor in $to_change
+                        if test -n "$$$monitor"
                             swww img -o (echo $$monitor | sed -e 's/_/-/g' ) -t any --transition-fps 120 $search_dir/$$$monitor
                         end
                     end
