5c229480070022537c67a254eb03a964e75e2398
rohrsben <rohrsben@gmail.com>
2025-02-19 01:00:30 -0500

    fix (nvim): grug-far no longer W12s

--- users/error/shared/apps/neovim/nvim/lua/autherror/plugins/grug-far.lua
+++ users/error/shared/apps/neovim/nvim/lua/autherror/plugins/grug-far.lua
@@ -1,17 +1,21 @@
 return {
     'MagicDuck/grug-far.nvim',
-    config = function()
-        require('grug-far').setup {
-        }
-    end,
     keys = {
         { '<leader>q', function ()
+            vim.cmd('write')
             require("grug-far").open {
                 transient = true,
                 prefills = {
                     paths = vim.fn.expand("%")
                 }
             }
-        end, desc = "Grug Far" }
+        end, desc = "Grug Far" },
+
+        { '<leader>Q', function ()
+            vim.cmd('wal')
+            require("grug-far").open {
+                transient = true,
+            }
+        end, desc = "Grug Farrer" }
     },
 }
