4c524e87a2496c84644780e71af2f754711487a1
rohrsben <rohrsben@gmail.com>
2025-03-10 21:36:45 -0400

    misc (config): more logical app list placement

--- users/error/autherror/apps/default.nix
+++ users/error/autherror/apps/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ inputs, pkgs, conf, ... }:
 
 {
     imports = [
@@ -13,4 +13,66 @@
         ./spotify.nix
         ./swww.nix
     ];
+
+    home = {
+        username = "error";
+        homeDirectory = "/home/error";
+        stateVersion = "${conf.stateVer}";
+
+        packages = with pkgs; [
+            bat
+            coreutils
+            eza
+            fd
+            gnused
+            lazygit
+            libnotify
+            libsForQt5.qt5.qtwayland
+            lua
+            p7zip
+            pass
+            python3
+            kdePackages.qtwayland
+            ripgrep
+            sops
+            xdg-utils
+
+            # command line
+            age
+            btop
+            cargo
+            gh
+            grim
+            hyprpicker
+            imagemagick
+            pandoc
+            playerctl
+            rsync
+            rustc
+            slurp
+            sshfs
+            tdf
+            ttyper
+            wev
+            zellij
+
+            # graphical
+            dolphin-emu
+            foliate
+            kdePackages.okular
+            localsend
+            mpv
+            pavucontrol
+            telegram-desktop
+            thunderbird
+
+            # supporting / desktop environment
+            udiskie
+            solaar
+            wl-clipboard
+            feh
+        ] ++ [
+            inputs.nh.packages.${pkgs.system}.default
+        ];
+    };
 }
--- users/error/autherror/default.nix
+++ users/error/autherror/default.nix
@@ -1,4 +1,4 @@
-{ pkgs, conf, config, inputs, lib, ... }:
+{ lib, pkgs, conf, config, ... }:
 
 lib.optionalAttrs(conf.hostName == "autherror") {
     sops.secrets.error-pass.neededForUsers = true;
@@ -37,67 +37,5 @@ lib.optionalAttrs(conf.hostName == "autherror") {
             ../shared/apps/yazi.nix
             ../shared/apps/zoxide.nix
         ];
-
-        home = {
-            username = "error";
-            homeDirectory = "/home/error";
-            stateVersion = "${conf.stateVer}";
-
-            packages = with pkgs; [
-                bat
-                coreutils
-                eza
-                fd
-                gnused
-                lazygit
-                libnotify
-                libsForQt5.qt5.qtwayland
-                lua
-                p7zip
-                pass
-                python3
-                kdePackages.qtwayland
-                ripgrep
-                sops
-                xdg-utils
-
-                # command line
-                age
-                btop
-                cargo
-                gh
-                grim
-                hyprpicker
-                imagemagick
-                pandoc
-                playerctl
-                rsync
-                rustc
-                slurp
-                sshfs
-                tdf
-                ttyper
-                wev
-                zellij
-
-                # graphical
-                dolphin-emu
-                foliate
-                kdePackages.okular
-                localsend
-                mpv
-                pavucontrol
-                telegram-desktop
-                thunderbird
-
-                # supporting / desktop environment
-                udiskie
-                solaar
-                wl-clipboard
-                feh
-            ] ++ [
-                inputs.nh.packages.${pkgs.system}.default
-            ];
-        };
     };
 }
