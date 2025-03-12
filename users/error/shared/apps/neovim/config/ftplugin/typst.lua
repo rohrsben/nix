56b730a021c684e00f31ce7776960e795a1270ec
rohrsben <rohrsben@gmail.com>
2025-03-11 23:07:18 -0400

    misc (nvim): minor typst ftplugin rework

--- users/error/shared/apps/neovim/config/ftplugin/typst.lua
+++ users/error/shared/apps/neovim/config/ftplugin/typst.lua
@@ -1,12 +1,15 @@
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true, silent = true }
+    if opts then
+        options = vim.tbl_extend('force', options, opts)
+    end
+    vim.keymap.set(mode, lhs, rhs, options)
+end
+
+map('i', '<C-n>', '<esc>viWsa$Ea', {noremap = false, remap = true})
+
 require('mini.pairs').map_buf(0, 'i', '$', {
     action = 'closeopen',
     pair = '$$',
     neigh_pattern = '[^\\]'
-})
-
-require('mini.pairs').map_buf(0, 'i', ' ', {
-    action = 'closeopen',
-    pair = '  ',
-    neigh_pattern = '[$][$]',
-    register = { bs = false }
 })
