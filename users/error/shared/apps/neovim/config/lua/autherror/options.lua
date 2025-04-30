3c6247f4e1b386fe938e5bb8fb23ef4fc18e34a9
rohrsben <rohrsben@gmail.com>
2025-04-30 03:45:14 -0400

    misc (nvim): simple autosave autocommand, relative numbers

--- users/error/shared/apps/neovim/config/lua/autherror/options.lua
+++ users/error/shared/apps/neovim/config/lua/autherror/options.lua
@@ -1,4 +1,4 @@
-vim.opt.scrolloff = 3
+vim.opt.scrolloff = 5
 vim.opt.sidescrolloff = 4
 
 vim.opt.wrap = true
@@ -12,10 +12,23 @@ vim.opt.expandtab = true
 vim.opt.timeoutlen = 500
 vim.opt.fileencoding = 'utf-8'
 
-vim.opt.swapfile = true
-vim.opt.updatetime = 300
+vim.opt.relativenumber = true
+
+-- autosave
+vim.api.nvim_create_autocmd({'BufLeave', 'FocusLost', 'VimLeavePre'}, {
+    callback = function (event)
+        local buf = event.buf
+        if vim.api.nvim_get_option_value('modified', {buf = buf}) then
+            vim.schedule(function ()
+                vim.api.nvim_buf_call(buf, function ()
+                    vim.cmd 'silent! write'
+                end)
+            end)
+        end
+    end
+})
 
 -- auto-resizes splits on terminal size change
-vim.api.nvim_create_autocmd("VimResized", {
-    command = "wincmd =",
+vim.api.nvim_create_autocmd('VimResized', {
+    command = 'wincmd =',
 })
