2ef27fd92b56f78666822b528555b15cbd55102e
rohrsben <rohrsben@gmail.com>
2025-06-09 23:45:23 -0400

    fix (nvim): update inline math snippet keybind

--- users/error/shared/apps/neovim/config/ftplugin/typst.lua
+++ users/error/shared/apps/neovim/config/ftplugin/typst.lua
@@ -4,4 +4,4 @@ require('mini.pairs').map_buf(0, 'i', '$', {
     neigh_pattern = '[^\\]%s'
 })
 
-vim.keymap.set('i', '<C-n>', function() MiniSnippets.default_insert({body = 'im'}) end, {desc = 'Inline Math Snippet'})
+vim.keymap.set('i', '<C-n>', function() MiniSnippets.default_insert({body = '$$1$'}) end, {desc = 'Inline Math Snippet'})
