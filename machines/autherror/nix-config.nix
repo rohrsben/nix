9c5d75a21ce81188afd2cebfb394567352801987
rohrsben <rohrsben@gmail.com>
2025-07-15 20:30:29 -0400

    misc (hypr): remove cachix

--- machines/autherror/nix-config.nix
+++ machines/autherror/nix-config.nix
@@ -6,8 +6,6 @@
     nix = {
         settings = {
             experimental-features = [ "nix-command" "flakes" ];
-            substituters = [ "https://hyprland.cachix.org" ];
-            trusted-public-keys = [ "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc=" ];
         };
 
         gc = {
