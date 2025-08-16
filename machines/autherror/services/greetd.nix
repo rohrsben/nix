d930c4bc502c65d385a92aff95a14d7635b015b0
rohrsben <rohrsben@gmail.com>
2025-08-16 02:04:43 -0400

    fix (boot): chase upstream package name

--- machines/autherror/services/greetd.nix
+++ machines/autherror/services/greetd.nix
@@ -6,8 +6,7 @@
 
         settings = {
             default_session = {
-                command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --cmd 'uwsm start default'";
-                # command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time";
+                command = "${pkgs.tuigreet}/bin/tuigreet --time --cmd 'uwsm start default'";
             };
         };
     };
