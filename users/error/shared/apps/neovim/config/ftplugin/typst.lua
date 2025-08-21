36536f1aba2ef25c9dc61b8b55ef98a93cd45d06
rohrsben <rohrsben@gmail.com>
2025-08-21 13:59:37 -0400

    update (nvim): typst ftplugin keybinds

--- users/error/shared/apps/neovim/config/ftplugin/typst.lua
+++ users/error/shared/apps/neovim/config/ftplugin/typst.lua
@@ -5,3 +5,6 @@ require('mini.pairs').map_buf(0, 'i', '$', {
 })
 
 vim.keymap.set('i', '<C-n>', function() MiniSnippets.default_insert({body = '$$1$'}) end, {desc = 'Inline Math Snippet'})
+
+vim.keymap.set('n', '<leader>ts', '<cmd>TypstPreview<cr>', {desc = "Start the typst preview"})
+vim.keymap.set('n', '<leader>tq', '<cmd>TypstPreviewStop<cr>', {desc = "Stop the typst preview"})
