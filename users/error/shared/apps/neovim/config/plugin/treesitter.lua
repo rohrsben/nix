4b8c370e9b3118a2642cefcca66a1715b3bef8c1
rohrsben <rohrsben@gmail.com>
2026-02-02 04:03:45 -0500

    fix (nvim): treesitter will remember this

--- users/error/shared/apps/neovim/config/plugin/treesitter.lua
+++ users/error/shared/apps/neovim/config/plugin/treesitter.lua
@@ -1,4 +1,7 @@
-vim.pack.add({'https://github.com/nvim-treesitter/nvim-treesitter'})
+vim.pack.add({{
+    src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+    version = 'main'
+}})
 
 vim.api.nvim_create_autocmd('FileType', {
     callback = function (info)
